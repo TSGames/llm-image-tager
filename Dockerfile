668ff007f52f60d98a3f4fa090f42cf99bb90427
torsten simon <anfragen@tsgames.de>
2025-06-01 22:38:18 +0200

    init
    image
    
    switch to ollama

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+FROM python:3.13-slim
+
+RUN apt-get update
+RUN apt-get install build-essential cmake libexiv2-dev libboost-python-dev -y
+
+# 2. Arbeitsverzeichnis im Container setzen
+WORKDIR /app
+
+# 3. Requirements zuerst kopieren und installieren (Docker Cache nutzen)
+COPY requirements.txt .
+
+RUN pip install --no-cache-dir -r requirements.txt
+
+
+# 4. Den restlichen Code ins Image kopieren
+COPY . .
+
+USER appuser
---
+++ llm.py
@@ -0,0 +1,87 @@
+import base64
+import json
+import os
+from io import BytesIO
+from pathlib import Path
+
+import ollama
+import pyexiv2
+from PIL import Image
+from PIL.Image import Resampling
+
+MODEL = os.getenv('MODEL', 'gemma3:4b')
+PROMPT = os.getenv('PROMPT', 'Erzeuge 5 bis 10 passende Schlagworte für dieses Bild in Deutsch.')
+FIXED_KEYWORD = os.getenv('FIXED_KEYWORD', 'LLM-Generated')
+OLLAMA_HOST = os.getenv('OLLAMA_HOST', 'http://ollama:11434')
+
+# docker
+ollama = ollama.Client(
+  host = OLLAMA_HOST,
+)
+
+class LLM:
+    def __init__(self):
+        ollama.pull(MODEL)
+
+    def image_to_base64_data_uri(self, image_path):
+        # Lade das Bild
+        # 896
+        image_resized = Image.open(image_path).resize((896, 896), resample=Resampling.BICUBIC).convert("RGB")
+        buffered = BytesIO()
+        image_resized.save(buffered, format="JPEG", quality=90)
+        img_bytes = buffered.getvalue()
+
+        # Bytes zu Base64 string
+        img_base64 = base64.b64encode(img_bytes).decode('utf-8')
+        return f"{img_base64}"
+
+    def classify_file(self, image_path: str):
+        metadata = pyexiv2.ImageMetadata(image_path)
+        metadata.read()
+        existing = metadata.get('Iptc.Application2.Keywords', None)
+        if existing and FIXED_KEYWORD in existing.value:
+            print('skipping tagging for ' + image_path)
+            return
+        print('tagging  ' + image_path)
+        data_uri = self.image_to_base64_data_uri(image_path)
+        #                ChatCompletionRequestUserMessage(role='user', content=PROMPT)
+        response = ollama.chat(
+            model=MODEL,
+            messages=[{
+                'role': 'user',
+                'content': PROMPT,
+                'images': [data_uri]
+            }],
+            format = {
+                "type": "object",
+                "properties": {
+                    "keywords": {
+                        "type": "array",
+                        "items": {
+                            "type": "string"
+                        }
+                    }
+                },
+                "required": [
+                    "keywords"
+                ]
+            }
+        )
+        keywords = json.loads(response.message.content)['keywords']
+        print(keywords)
+        if len(keywords) > 0:
+            keywords = keywords + [FIXED_KEYWORD]
+            if existing:
+                keywords = list(set(keywords) | set(existing.value))
+            print(keywords)
+            metadata['Iptc.Application2.Keywords'] = keywords
+            metadata.write()
+
+    def classify_folder(self, folder_path):
+        jpeg_files = [f for f in Path(folder_path).rglob("*") if f.suffix.lower() in ['.jpg', '.jpeg']]
+        for file in jpeg_files:
+            self.classify_file(str(file))
+
+
+
+LLM().classify_folder("/mnt/share2/2025/05")
