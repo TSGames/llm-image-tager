e21a3b1cef7dcd76a0e8816ae4a501a20cf5e346
torsten simon <anfragen@tsgames.de>
2025-06-03 20:42:47 +0200

    Dockerfile

--- llm.py
+++ llm.py
@@ -15,7 +15,9 @@ MODEL = os.getenv('MODEL', 'gemma3:4b')
 PROMPT = os.getenv('PROMPT', 'Erzeuge 5 bis 10 passende Schlagworte für dieses Bild in Deutsch.')
 FIXED_KEYWORD = os.getenv('FIXED_KEYWORD', 'LLM-Generated')
 OLLAMA_HOST = os.getenv('OLLAMA_HOST', 'http://ollama:11434')
+PATH = os.getenv('PATH', '/mnt/images')
 SLEEP_DURATION_SEC = os.getenv('SLEEP_DURATION_SEC', 60)
+IMAGE_SIZE = os.getenv('IMAGE_SIZE', '128')
 
 
 logging.basicConfig(
@@ -35,7 +37,7 @@ class LLM:
     def image_to_base64_data_uri(self, image_path):
         # Lade das Bild
         # 896
-        image_resized = Image.open(image_path).resize((896, 896), resample=Resampling.BICUBIC).convert("RGB")
+        image_resized = Image.open(image_path).resize((IMAGE_SIZE, IMAGE_SIZE), resample=Resampling.BICUBIC).convert("RGB")
         buffered = BytesIO()
         image_resized.save(buffered, format="JPEG", quality=90)
         img_bytes = buffered.getvalue()
@@ -49,9 +51,9 @@ class LLM:
         metadata.read()
         existing = metadata.get('Iptc.Application2.Keywords', None)
         if existing and FIXED_KEYWORD in existing.value:
-            logging.info('skipping tagging for ' + image_path)
+            logging.info('skipping classifying for ' + image_path)
             return
-        logging.info('tagging  ' + image_path)
+        logging.info('classifying  ' + image_path)
         data_uri = self.image_to_base64_data_uri(image_path)
         #                ChatCompletionRequestUserMessage(role='user', content=PROMPT)
         response = ollama.chat(
@@ -90,11 +92,13 @@ class LLM:
     def classify_folder(self, folder_path):
         jpeg_files = [f for f in Path(folder_path).rglob("*") if f.suffix.lower() in ['.jpg', '.jpeg']]
         for file in jpeg_files:
+            if any(part == "@eaDir" for part in file.parts):
+                continue
             self.classify_file(str(file))
         logging.info("Finished classifying " + str(len(jpeg_files)) + " images")
 
 
 while True:
-    LLM().classify_folder("/mnt/images")
+    LLM().classify_folder(PATH)
     logging.info("Sleeping for " + str(SLEEP_DURATION_SEC) + " seconds")
     time.sleep(SLEEP_DURATION_SEC)
