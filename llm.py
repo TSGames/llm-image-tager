8b9490abdb43080b70edf4288d917cf13b7995e2
torsten simon <anfragen@tsgames.de>
2025-06-04 20:41:00 +0200

    tagging

--- llm.py
+++ llm.py
@@ -13,11 +13,14 @@ from PIL.Image import Resampling
 
 MODEL = os.getenv('MODEL', 'gemma3:4b')
 PROMPT = os.getenv('PROMPT', 'Erzeuge 5 bis 10 passende Schlagworte für dieses Bild in Deutsch.')
-FIXED_KEYWORD = os.getenv('FIXED_KEYWORD', 'LLM-Generated')
+FIXED_TAG = os.getenv('FIXED_TAG', 'LLM-Generated')
+KEEP_EXISTING_TAGS = os.getenv('KEEP_EXISTING_TAGS', True)
+# Skip images already having tags but not the FIXED_TAG assigned
+SKIP_MANUALLY_TAGGED = os.getenv('SKIP_MANUALLY_TAGGED', True)
 OLLAMA_HOST = os.getenv('OLLAMA_HOST', 'http://ollama:11434')
 IMAGE_PATH = os.getenv('IMAGE_PATH', '/mnt/images')
 SLEEP_DURATION_SEC = os.getenv('SLEEP_DURATION_SEC', 60)
-IMAGE_SIZE = os.getenv('IMAGE_SIZE', 128)
+IMAGE_SIZE = os.getenv('IMAGE_SIZE', 896)
 
 
 logging.basicConfig(
@@ -36,8 +39,8 @@ class LLM:
 
     def image_to_base64_data_uri(self, image_path):
         # 896
-        logging.info('resizing to ' + str(IMAGE_SIZE))
-        image_resized = Image.open(image_path).resize((IMAGE_SIZE, IMAGE_SIZE), resample=Resampling.BICUBIC).convert("RGB")
+        logging.info('resizing to ' + str(KEEP_EXISTING_TAGS))
+        image_resized = Image.open(image_path).resize((KEEP_EXISTING_TAGS, KEEP_EXISTING_TAGS), resample=Resampling.BICUBIC).convert("RGB")
         buffered = BytesIO()
         image_resized.save(buffered, format="JPEG", quality=90)
         img_bytes = buffered.getvalue()
@@ -51,8 +54,11 @@ class LLM:
         metadata = pyexiv2.ImageMetadata(image_path)
         metadata.read()
         existing = metadata.get('Iptc.Application2.Keywords', None)
-        if existing and FIXED_KEYWORD in existing.value:
+        if existing and FIXED_TAG in existing.value:
             logging.info('skipping classifying for ' + image_path)
+            return
+        if SKIP_MANUALLY_TAGGED and len(existing.value) > 0:
+            logging.info('skipping manually taged file ' + image_path)
             return
         logging.info('classifying  ' + image_path)
         data_uri = self.image_to_base64_data_uri(image_path)
@@ -82,8 +88,8 @@ class LLM:
         keywords = json.loads(response.message.content)['keywords']
         logging.info(keywords)
         if len(keywords) > 0:
-            keywords = [k.capitalize() for k in keywords] + [FIXED_KEYWORD]
-            if existing:
+            keywords = [k.capitalize() for k in keywords] + [FIXED_TAG]
+            if KEEP_EXISTING_TAGS and existing:
                 keywords = list(set(keywords) | set(existing.value))
             logging.info(keywords)
             metadata['Iptc.Application2.Keywords'] = keywords
